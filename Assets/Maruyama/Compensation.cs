46262d89b5d85f925cfe9bcaeee49df431d1cd65
mkgi16cd <mkgi16cd@gmail.com>
2025-06-06 17:25:41 +0900

    [add]代償攻撃を実装しました。

---
+++ Assets/Maruyama/Compensation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "UniqueEffects/Compensation")]
+public class Compensation : UniqueEffect
+{
+    [SerializeField] float magnification = 1.5f; //ダメージの倍率を設定
+    [SerializeField] int damageteisuu = 10; //代償
+    //カードの効果処理
+    public override void Execute(Card card, Card flontCard, Battler player, Enemy enemy, Text message)
+    {
+        int attackValue = FlontBuff(card, flontCard);
+
+        int Hit = (int)(attackValue * magnification * Random.Range(0.8f, 1.2f));
+        float defense = 1f - enemy.Base.EnemyDefense / 100f;
+        int damage = (int)(Hit * defense);
+        enemy.Base.EnemyLife -= damage;
+        if (enemy.Base.EnemyLife < 0)
+        {
+            enemy.Base.EnemyLife = 0;
+        }
+        player.Life -= damageteisuu;
+        if (player.Life < 0)
+        {
+            player.Life = 0;
+        }
+        message.text = $"{damage}ダメージ与え、\n{damageteisuu}ダメージの代償を受けた";
+    }
+    //一枚前のカードの追加効果処理
+    public int FlontBuff(Card card, Card flontCard)
+    {
+
+        float attackValue = (int)card.Base.CardStatus.Attack_Status;
+
+
+        if (flontCard == null)
+        {
+            return (int)attackValue;
+        }
+        else
+        {
+            string cardName = flontCard.Base.CardName;
+            FlontBuff foundBuff = card.Base.FlontBuff.Find(buff => buff.flontCard == cardName);
+
+            if (foundBuff == null)
+            {
+                return (int)attackValue;
+            }
+            attackValue *= foundBuff.buff;
+            return (int)attackValue;
+        }
+
+
+    }
+}
